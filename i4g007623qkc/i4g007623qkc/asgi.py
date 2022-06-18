b731de61af6eb74f0402001a97e29a3fa9b062f8
showgologo <showgologo@localhost.localdomain>
2022-06-18 16:00:35 +0100

    Django Models

---
+++ i4g007623qkc/i4g007623qkc/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for i4g007623qkc project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'i4g007623qkc.settings')
+
+application = get_asgi_application()
